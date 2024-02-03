fc32ff956627630028972f97376985f264706b72
kokareamol <115057222+kokareamol@users.noreply.github.com>
2024-02-03 23:13:50 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "amol-ajaykumartetris-bucket" # Replace with your actual S3 bucket name
+    bucket = "amol-ajaykumartetris-bucket2" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-southeast-2"
   }
